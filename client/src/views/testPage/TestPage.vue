b20e5b932a0a5e76d33d814124aee1fdef9f81d1
Нерода Максим <neroda@isands.ru>
2021-10-05 17:55:57 +0700

    Настроено юнит тестирование(самих тестов пока нет)

---
+++ client/src/views/testPage/TestPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <span class='test-selector'>
+        <h1>Hello</h1>
+    </span>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class TestPage extends Vue {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
